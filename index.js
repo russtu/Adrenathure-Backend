eb436d63f9d27d8d13d1fa0b0cdb158b6530b639
cmmiguel <cmmiguel@gmail.com>
2022-01-03 13:07:04 +0100

    Endpoints places

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const mysqlUsersRepository = require('./repositories/mysql/mysqlUsersRepository')
-
-
+const mysqlPlacesRepository = require('./repositories/mysql/mysqlPlacesRepository')
 
 
 const app = express()
@@ -46,6 +45,80 @@ app.post('/users', async (req, res) => {
 })
 
 
+
+
+// 10. destinos. GET /places. Retorna: todos los places y los places donde recommended === true
+
+app.get('/places', async (req, res) => {
+    let places
+    try {
+        places = await mysqlPlacesRepository.getPlaces()
+
+    } catch (error) {
+        res.status('500')
+        res.end(error)
+        return
+    }
+
+    if (!places) {
+        res.status(404)
+        res.end('No places found')
+        return
+    }
+
+    res.status(200)
+    res.send(places)
+})
+
+
+// 11. destinos -> destino recomendado. GET /places/recommended . Retorna: todos los places destacados (recommeded === true) y el mapa
+
+app.get('/places/recommended', async (req, res) => {
+
+    let places
+    try {
+        places = await mysqlPlacesRepository.getPlacesRecommended()
+
+    } catch (error) {
+        res.status('500')
+        res.end(error)
+        return
+    }
+
+    if (!places) {
+        res.status(404)
+        res.end('No places')
+        return
+    }
+
+    res.status(200)
+    res.send(places)
+})
+
+
+// 12. destinos -> destino en concreto. GET /places/:placeId. Retorna: todos place en concreto con las experiencias pertenecientes a ete destino
+
+app.get('/places/:placeId', async (req, res) => {
+    //const places = await mysqlPlacesRepository.getPlacesById(req.params.placeId)
+    const { placeId } = req.params
+    let place
+    try {
+        place = await mysqlPlacesRepository.getPlacesById(placeId)
+    } catch(error) {
+        res.status('500')
+        res.end(error)
+        return
+    }
+
+    if (!place) {
+        res.status(404)
+        res.end('No places')
+        return
+    }
+
+    res.status(200)
+    res.send(place)
+})
 
 
 
