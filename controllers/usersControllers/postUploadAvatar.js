499d0b0cbb424047f0a660304524d1d9867a5a84
Rubén <rubcalop@gmail.com>
2022-01-18 16:55:21 +0100

    resolved  bug fileupload

--- controllers/usersControllers/postUploadAvatar.js
+++ controllers/usersControllers/postUploadAvatar.js
@@ -1,20 +1,9 @@
-const express = require('express')
-const fileUpload = require('express-fileupload')
-
-const app = express()
-
-app.use(express.json())
-
-app.use('/public', express.static('uploads'))
-app.use(fileUpload())
-
-
 const postUploadAvatar = (req, res) => {
     const userId = req.user.id
     console.log(userId)
     const avatar = req.files.avatar
 
-    avatar.mv(`../../uploads/${userId}-${avatar.name}`)
+    avatar.mv(`${__dirname}/../../uploads/${userId}-${avatar.name}`)
 
     res.send('File uploaded successfully')
 }
